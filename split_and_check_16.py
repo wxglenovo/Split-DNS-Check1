bd5101a5a012445fd022f1fe9cb7673f13e62025
wxglenovo <148731588+wxglenovo@users.noreply.github.com>
2025-11-21 22:56:52 +0800

    Update split_and_check_16.py

--- split_and_check_16.py
+++ split_and_check_16.py
@@ -344,8 +344,6 @@ def dns_validate(rules, part):
             res = future.result()
             if res:
                 valid_rules.append(res)
-            else:
-            #   print(f"⚠ 验证失败: {future.result()}")  # 输出失败的规则
             completed += 1
             
             # 输出进度信息
